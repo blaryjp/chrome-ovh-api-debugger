d2dfaf589e758fbbe53c1b202649fe4c2c4282dd
jblary <contact@blary.be>
2014-06-29 22:33:35 +0200

    goToSpaces

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 // onMessage, if from console, eval function to call the right console fct with args
 chrome.extension.onMessage.addListener(function (request) {
-	if (request.command === 'console') {
+    if (request.command === 'console') {
         chrome.tabs.executeScript(request.tabId, {
             code: "(function (type, args) { " +
                       "console[type].apply(console, JSON.parse(decodeURI(args))); " +
